5d5c81ab3e51f347312118831e8a2b470ac6c942
loucass003 <loucass003@gmail.com>
2017-09-16 19:43:44 +0200

    [Admin] Finish Add anime design

--- src/components/animes/add.vue
+++ src/components/animes/add.vue
@@ -1,5 +1,8 @@
 <template>
   <v-container>
+    <div class="text-xs-center">
+      <h2 class="headline">Add Anime</h2>
+    </div>
     <v-layout>
       <v-flex xs6 offset-xs3>
         <v-text-field
@@ -43,29 +46,137 @@
           </template>
         </v-select>
       </v-flex>
+      <v-flex xs6>
+        <v-select
+          label="Select Authors"
+          :items="authors"
+          v-model="selectedAuthors"
+          item-text="name"
+          item-value="id"
+          class="tags-select"
+          multiple
+          chips
+          autocomplete
+        >
+          <template slot="selection" scope="data">
+            <v-chip
+              close
+              @input="data.parent.selectItem(data.item.id)"
+              @click.native.stop
+              class="chip--select-multi"
+              :key="data.item.id"
+            >
+              <v-list-tile-avatar :style="{ 'background-color': data.item.color }"></v-list-tile-avatar>
+              {{ data.item.name }}
+            </v-chip>
+          </template>
+          <template slot="item" scope="data">
+            <v-list-tile-avatar>
+              <div :style="{ 'background-color': data.item.color, 'width': '30px', 'height': '30px', 'border-radius': '50%' }"></div>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+            </v-list-tile-content>
+          </template>
+        </v-select>
+      </v-flex>
     </v-layout>
+    <v-layout>
+      <v-flex xs6>
+        <v-select
+          :items="animesStatus"
+          v-model="selectedStatus"
+          label="Status"
+        ></v-select>
+      </v-flex>
+      <v-flex xs6>
+        <v-menu
+          lazy
+          :close-on-content-click="false"
+          v-model="menu"
+          transition="scale-transition"
+          offset-y
+          full-width
+          :nudge-left="40"
+          max-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            label="Release Date"
+            v-model="date"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title scrollable actions>
+            <template scope="{ save, cancel }">
+              <v-card-actions>
+                <v-btn flat primary @click.native="cancel()">Cancel</v-btn>
+                <v-btn flat primary @click.native="save()">Save</v-btn>
+              </v-card-actions>
+            </template>
+          </v-date-picker>
+        </v-menu>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs5>
+        <v-text-field
+          label="Cover Link"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs1>
+        <v-btn icon large primary @click.stop="">
+          <v-icon>file_upload</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs5>
+        <v-text-field
+          label="Banner Link"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs1>
+        <v-btn icon large primary @click.stop="">
+          <v-icon>file_upload</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <v-text-field
+          label="Desc"
+          multi-line
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layou>
+      <v-flex offset-xs10 xs2 class="text-xs-right">
+        <v-btn primary @click.stop="">
+          Add
+        </v-btn>
+      </v-flex>
+    </v-layou>
   </v-container>
 </template>
 
 <script>
-  import { VDataTable, VIcon, VBtn, VDialog, VTextField, VSelect, VChip, VAvatar } from 'vuetify/src/components'
+  import { VDataTable, VIcon, VBtn, VDialog, VTextField, VSelect, VChip, VAvatar, VDatePicker, VMenu } from 'vuetify/src/components'
   import { VList, VListGroup, VListTile, VListTileTitle, VListTileAction, VListTileContent, VListTileAvatar } from 'vuetify/src/components/VList'
   import { VContainer, VFlex, VLayout } from 'vuetify/src/components/VGrid'
-  import Datepicker from '../../picker/datepicker.vue'
-
-
-
   import gql from 'graphql-tag'
 
-  export default {
+  export default
+  {
     name: 'animes_add',
     data () {
       return {
-        date: {
-          time: ''
-        },
+        date: null,
+        menu: false,
+        authors: [],
         tags: [],
         selectedTags: [],
+        selectedAuthors: [],
         animesStatus: [],
         selectedStatus: ''
       }
@@ -90,7 +201,8 @@
       VListTileContent,
       VListTileAvatar,
       VAvatar,
-      Datepicker
+      VMenu,
+      VDatePicker
     },
     apollo: {
       animesStatus: {
@@ -108,25 +220,20 @@
       tags: {
         query: gql`{ tags { id name color desc } }`,
         update: ({ tags }) => tags
+      },
+      authors: {
+        query: gql`{ authors { id name color desc } }`,
+        update: ({ authors }) => authors
       }
     }
   }
 </script>
 
 <style lang="stylus">
-  @import '../../stylus/main.styl'
+  @import '../../stylus/main.styl';
 
-  .color-result {
-    display: inline-block;
-    height: 35px;
-    width: 35px;
-    border-radius: 50%;
-    border: solid 1px #999;
-    cursor: pointer;
-  }
-
-  .color-picker {
-    width: initial !important;
+  .flex {
+    padding: 5px;
   }
 
   .tags-select > .input-group__selections {
