23e8eb869d59b7ebec1e365d403b7bce937c426a
Duarte David <deltaduartedavid@gmail.com>
2018-05-19 23:14:23 +0200

    Use @popcorn.moe/apollo

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,25 +1,10 @@
 import Vue from "vue";
-import { ApolloClient } from "apollo-client";
-import { createUploadLink } from "apollo-upload-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
 import VueApollo from "vue-apollo";
+import createClient from "@popcorn.moe/apollo";
 
 Vue.use(VueApollo);
 
-const cache = new InMemoryCache({
-	addTypename: true,
-	dataIdFromObject: ({ id }) => id
-});
-
-const link = createUploadLink({
-	uri: `${process.env.API_URL}/graphql`,
-	credentials: "include"
-});
-
-const client = new ApolloClient({
-	link,
-	cache
-});
+const client = createClient();
 
 export default new VueApollo({
 	defaultClient: client
